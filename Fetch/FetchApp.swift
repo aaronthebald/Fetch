2daa2773088bb12d976a8988a801d6ae6fdad312
Aaron Wilson <124102001+aaronthebald@users.noreply.github.com>
2025-02-15 10:37:44 -0500

    Initial Commit

---
+++ Fetch/FetchApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchApp.swift
+//  Fetch
+//
+//  Created by Aaron Wilson on 2/15/25.
+//
+
+import SwiftUI
+
+@main
+struct FetchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
