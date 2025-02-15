61e508cd60aa958f70d0d64f8fd1c887369b7bff
Aaron Wilson <124102001+aaronthebald@users.noreply.github.com>
2025-02-15 11:15:43 -0500

    Inital Commit. I added a file for the data model as well as a testing target to the app.

---
+++ FetchTests/FetchTests.swift
@@ -0,0 +1,16 @@
+//
+//  FetchTests.swift
+//  FetchTests
+//
+//  Created by Aaron Wilson on 2/15/25.
+//
+
+import Testing
+
+struct FetchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
